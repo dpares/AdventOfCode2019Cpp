803e8f706c5b7e0ad9f0fd9d5c376070e5ed5ced
Daniel Parés Aguilar <dparesag@gmail.com>
2019-12-01 21:00:32 +0100

    Day 1

---
+++ AdventOfCode2019/aoc1A.cpp
@@ -0,0 +1,54 @@
+#include "pch.h"
+
+int requiredFuel(int mass)
+{
+	return std::max(0, static_cast<int>(mass / 3.0f) - 2);
+}
+
+int challengeA(std::ifstream& input)
+{
+	int moduleMass = 0;
+	int fuelSum = 0;
+
+	while (input >> moduleMass)
+	{
+		const int moduleFuel = requiredFuel(moduleMass);
+		fuelSum += moduleFuel;
+	}
+
+	return fuelSum;
+}
+
+int challengeB(std::ifstream& input)
+{
+	int moduleMass = 0;
+	int fuelSum = 0;
+
+	while (input >> moduleMass)
+	{
+		int moduleFuel = requiredFuel(moduleMass);
+		while (moduleFuel > 0)
+		{
+			fuelSum += moduleFuel;
+			moduleFuel = requiredFuel(moduleFuel);
+		}
+	}
+
+	return fuelSum;
+}
+
+int main()
+{
+	std::ifstream input ("input/day1.txt", std::ifstream::in);
+
+	int totalFuel = challengeA(input);
+	std::cout << "Total fuel for challenge A: " << totalFuel << std::endl;
+
+	input.clear();
+	input.seekg(0);
+
+	totalFuel = challengeB(input);
+	std::cout << "Total fuel for challenge B: " << totalFuel << std::endl;
+}
+
+
